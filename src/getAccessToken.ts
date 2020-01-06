4badf346270ab30c3d49cd63271f0acb272fd918
Nick Larew <nick.larew@10gen.com>
2020-01-06 14:52:16 -0500

    setup

--- src/getAccessToken.ts
+++ src/getAccessToken.ts
@@ -5,18 +5,18 @@ import axios from "axios";
 export default async function getAccessToken(mode = "stitch") {
   console.log("getAccessToken");
   const result = await axios({
+    method: "post",
     url: `https://${mode}.mongodb.com/api/admin/v3.0/auth/providers/mongodb-cloud/login`,
     headers: {
-      "Cache-Control": "no-cache",
       "Content-Type": "application/json",
       Accept: "application/json",
+      FooBar: "asdfasdfasdf"
     },
     data: {
       username: "CFSEPXSJ",
       apiKey: "42f54c8d-ea4d-460a-8fcc-031136ca3bfe",
     },
   });
-
   console.log("result", result);
   return result;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,33 @@
+import * as React from "react";
 import { render } from "react-dom";
-
 import "./styles.css";
 
-// import { AppRegistry } from 'react-native';
-// AppRegistry.registerComponent("MyApplication", () => App);
-
-import * as React from "react";
 import gql from "graphql-tag";
 import { HttpLink, InMemoryCache, ApolloLink } from "apollo-boost";
 import { ApolloClient } from "apollo-client";
 import { ApolloProvider, useQuery } from "@apollo/react-hooks";
-
-import getAccessToken from "./getAccessToken";
-
-getAccessToken("stitch-dev")
-  .then(result => console.log("woohoOoo"))
-  .catch(err => console.error("fack", err));
+import { setContext } from "apollo-link-context"
 
 const REALM_GRAPHQL_URL =
   "https://stitch-dev.mongodb.com/api/admin/v3.0/groups/5dd58ffb7a3e5a6c5bce88da/apps/5dd81bbe751ac96d376b5284/graphql";
 
 const ACCESS_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzY4MTUxMTYsImlhdCI6MTU3NjgxMzMxNiwiaXNzIjoiNWRmYzQzMDQ2MmIzNjJmMzg3NmQ5Yjg5Iiwic3RpdGNoX2RhdGEiOnsiYXBwX2lkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwiYXV0aC9zb3VyY2VkX2J5X3Byb3ZpZGVyX2lkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwibW9uZ29kYi9jbG91ZC1hcGlLZXkiOiI0MmY1NGM4ZC1lYTRkLTQ2MGEtOGZjYy0wMzExMzZjYTNiZmUiLCJtb25nb2RiL2Nsb3VkLXVzZXJuYW1lIjoiQ0ZTRVBYU0oifSwic3RpdGNoX2RldklkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwic3RpdGNoX2RvbWFpbklkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwic3ViIjoiNWRmYzM4ZDI3YTNlNWEyOTY5OGM0ZDgwIiwidHlwIjoiYWNjZXNzIn0.vTJNcA4mi_BqI033ab1sUBQ1Eggi8NX5emfaL1ymXQ8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzgzNDExODYsImlhdCI6MTU3ODMzOTM4NiwiaXNzIjoiNWUxMzhjM2ExYjM4ZDM1ODQzMGVhYjE4Iiwic3RpdGNoX2RhdGEiOnsiYXBwX2lkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwiYXV0aC9zb3VyY2VkX2J5X3Byb3ZpZGVyX2lkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwibW9uZ29kYi9jbG91ZC1hcGlLZXkiOiI0MmY1NGM4ZC1lYTRkLTQ2MGEtOGZjYy0wMzExMzZjYTNiZmUiLCJtb25nb2RiL2Nsb3VkLXVzZXJuYW1lIjoiQ0ZTRVBYU0oifSwic3RpdGNoX2RldklkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwic3RpdGNoX2RvbWFpbklkIjoiMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwIiwic3ViIjoiNWRmYzM4ZDI3YTNlNWEyOTY5OGM0ZDgwIiwidHlwIjoiYWNjZXNzIn0.vDx9bxec4pjPtA74Ko77Z6QyvMKGq9XNnZ2eU3H5l08"
 
-const authLink = new ApolloLink((operation, forward) => {
-  operation.setContext({
+const authLink = setContext((_, { headers }) => {
+  return {
     headers: {
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
-      FooBar: "MyFaceWhen",
+      ... headers,
+      Authorization: `Bearer ${ACCESS_TOKEN}`
     },
-  });
-  // Call the next link in the middleware chain.
-  return forward(operation);
-});
+  }
+})
 
-const httpLink = new HttpLink({
-  uri: REALM_GRAPHQL_URL,
-  // credentials: "same-origin",
-});
+const httpLink = new HttpLink({ uri: REALM_GRAPHQL_URL });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
 
 const GET_ALL_MOVIES = gql`
@@ -56,7 +42,9 @@ const GET_ALL_MOVIES = gql`
 function App() {
   const { loading, error, data } = useQuery(GET_ALL_MOVIES);
 
+  // Log anytime data or loading state changes
   React.useEffect(() => {
+    console.log("data/loading change");
     console.log("loading", loading);
     console.log("data", data);
   }, [data, loading]);
@@ -71,7 +59,7 @@ function App() {
     </div>
   );
 }
-console.log("client", client);
+
 render(
   <ApolloProvider client={client}>
     <App />
